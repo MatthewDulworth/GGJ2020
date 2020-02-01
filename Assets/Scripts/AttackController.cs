914618f74cb4ca1853dca0d1dd516febab941c39
Alexhwa <ah.alswad@gmail.com>
2020-01-31 22:32:27 -0600

    Scripts and physics

---
+++ Assets/Scripts/AttackController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Attack", menuName = "Attack")]
+public class AttackController : MonoBehaviour
+{
+    //What angle enemies will fly back at
+    public Vector2 knockbackAngle;
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Attack")
+        {
+            AttackController attack = collision.gameObject.GetComponent<AttackController>();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

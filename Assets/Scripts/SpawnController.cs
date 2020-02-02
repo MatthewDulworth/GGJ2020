29e0724a36b5b4461b254fa5e2cb0187f00dd19e
Alexhwa <ah.alswad@gmail.com>
2020-02-01 22:55:51 -0600

    Porgress

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject spawnPoints;
+    public float spawnRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
